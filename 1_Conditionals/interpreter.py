15308d392fef9a301ea7122782904f6de2311e7a
Aleksandra Jercha <aleks.jercha@gmail.com>
2024-07-21 15:23:59 +0200

    Add interpreter.py file

---
+++ 1_Conditionals/interpreter.py
@@ -0,0 +1,34 @@
+"""
+In a file called interpreter.py, implement a program that prompts the user for an arithmetic expression and then calculates and outputs the result as a floating-point value formatted to one decimal place. Assume that the user’s input will be formatted as x y z, with one space between x and y and one space between y and z, wherein:
+
+x is an integer
+y is +, -, *, or /
+z is an integer
+For instance, if the user inputs 1 + 1, your program should output 2.0. Assume that, if y is /, then z will not be 0.
+"""
+
+
+def main():
+    expression = input("Expression: ")
+
+    x, y, z = expression.split(" ")
+
+    x = int(x)
+    z = int(z)
+
+    if y == "+":
+        result = x + z
+    elif y == "-":
+        result = x - z
+    elif y == "*":
+        result = x * z
+    elif y == "-":
+        result = x / z
+    else:
+        print("Invalid operator")
+        return
+
+    print(f"{result:.1f}")
+
+
+main()
